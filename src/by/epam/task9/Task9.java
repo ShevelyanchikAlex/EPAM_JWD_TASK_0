e16598210789b3630e5eedab010cc30bc4c3e75b
ShevelyanchikAlex <shevelyanchik01@mail.ru>
2021-09-30 13:31:41 +0300

    Refactor task9.

--- src/by/epam/task9/Task9.java
+++ src/by/epam/task9/Task9.java
@@ -1,16 +1,23 @@
 package by.epam.task9;
 
+import by.epam.services.reader.impl.ReaderImpl;
+import by.epam.services.reader.interfaces.Reader;
+import by.epam.services.writer.impl.WriterImpl;
+import by.epam.services.writer.interfaces.Writer;
+
 import java.util.Arrays;
 
 public class Task9 {
     public static void main(String[] args) {
-        int[] arr1 = {1, 2, 3, 4};
-        int[] arr2 = {-3, -2, -1};
-        int k = 2;
+        Reader reader = new ReaderImpl();
+        Writer writer = new WriterImpl();
+
+        int[] arr1 = inputNumbers(4, reader);
+        int[] arr2 = inputNumbers(4, reader);
+        int k = reader.readInt();
         int[] arr3 = new int[arr1.length + arr2.length];
 
-        int i = 0;
-        for (int j = 0; j < arr1.length; j++) {
+        for (int i = 0, j = 0; j < arr1.length; j++) {
             if ((j - 1) == k) {
                 for (int num : arr2) {
                     arr3[i++] = num;
@@ -20,6 +27,16 @@ public class Task9 {
             }
         }
 
-        System.out.println(Arrays.toString(arr3));
+        writer.writeLn(Arrays.toString(arr3));
+    }
+
+
+    private static int[] inputNumbers(int number, Reader reader) {
+        int[] arr = new int[number];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = reader.readInt();
+        }
+        return arr;
     }
 }
