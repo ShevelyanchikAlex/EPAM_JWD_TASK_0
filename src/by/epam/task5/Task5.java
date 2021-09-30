9fa5eb90bf815ad04d3439be6bd2221ae1210fe8
ShevelyanchikAlex <shevelyanchik01@mail.ru>
2021-09-30 13:31:10 +0300

    Refactor task5.

--- src/by/epam/task5/Task5.java
+++ src/by/epam/task5/Task5.java
@@ -1,16 +1,33 @@
 package by.epam.task5;
 
+import by.epam.services.reader.impl.ReaderImpl;
+import by.epam.services.reader.interfaces.Reader;
+import by.epam.services.writer.impl.WriterImpl;
+import by.epam.services.writer.interfaces.Writer;
+
 public class Task5 {
 
     public static void main(String[] args) {
-        double[] arr = {-2, 3, 4};
+        Reader reader = new ReaderImpl();
+        Writer writer = new WriterImpl();
+        double[] arr = inputNumbers(3, reader);
 
         for (double v : arr) {
             if (v >= 0) {
-                System.out.println(Math.pow(v, 2));
+                writer.writeLn(Math.pow(v, 2));
             } else {
-                System.out.println(Math.pow(v, 4));
+                writer.writeLn(Math.pow(v, 4));
             }
         }
     }
+
+    private static double[] inputNumbers(int number, Reader reader) {
+        double[] arr = new double[number];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = reader.readDouble();
+        }
+        return arr;
+    }
+
 }
