5ad38af63252a38618e268f03c99291a0896616a
ShevelyanchikAlex <shevelyanchik01@mail.ru>
2021-09-30 13:30:46 +0300

    Refactor task3.

--- src/by/epam/task3/Task3.java
+++ src/by/epam/task3/Task3.java
@@ -1,13 +1,22 @@
 package by.epam.task3;
 
+import by.epam.services.reader.impl.ReaderImpl;
+import by.epam.services.reader.interfaces.Reader;
+import by.epam.services.writer.impl.WriterImpl;
+import by.epam.services.writer.interfaces.Writer;
+
 public class Task3 {
     public static void main(String[] args) {
-        double a = 3;
-        double b = 4;
+        Reader reader = new ReaderImpl();
+        Writer writer = new WriterImpl();
+
+        double a = reader.readDouble();
+        double b = reader.readDouble();
 
         double perimeter = calculatePerimeter(a, b);
         double square = calculateSquare(a, b);
-        System.out.println("Perimeter = " + perimeter + ", Square = " + square);
+
+        writer.write("Perimeter = " + perimeter + ", Square = " + square);
     }
 
     private static double calculatePerimeter(double a, double b) {
