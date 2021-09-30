9b7b73e10ef8dc7f9da05e37bcbc1dee04497413
ShevelyanchikAlex <shevelyanchik01@mail.ru>
2021-09-30 13:30:23 +0300

    Refactor task2.

--- src/by/epam/task2/Task2.java
+++ src/by/epam/task2/Task2.java
@@ -1,24 +1,40 @@
 package by.epam.task2;
 
+import by.epam.services.reader.impl.ReaderImpl;
+import by.epam.services.reader.interfaces.Reader;
+import by.epam.services.writer.impl.WriterImpl;
+import by.epam.services.writer.interfaces.Writer;
+
 public class Task2 {
     public static void main(String[] args) {
-        double a = 1;
-        double b = 4;
-        double c = 5;
+        Reader reader = new ReaderImpl();
+        Writer writer = new WriterImpl();
 
-        double result = calculateExpression(a, b, c);
-        System.out.println(result);
+        double a = reader.readDouble();
+        double b = reader.readDouble();
+        double c = reader.readDouble();
+
+        try {
+            double result = calculateExpression(a, b, c);
+            writer.write(result);
+        } catch (ArithmeticException e) {
+            writer.writeLn(e.getMessage());
+        }
     }
 
-    private static double calculateExpression(double a, double b, double c) {
+    private static double calculateExpression(double a, double b, double c) throws ArithmeticException {
         double result;
 
-        if (a == 0) {
-            throw new java.lang.ArithmeticException("Calculation failed, denominator is 0");
-        } else {
+        if (isDenominatorValid(a)) {
             result = (b + Math.sqrt(b * b + 4 * a * c)) / (2 * a) - Math.pow(a, 3) * c + Math.pow(b, -2);
+        } else {
+            throw new java.lang.ArithmeticException("Calculation failed, denominator is 0");
         }
         return result;
+    }
+
+    private static boolean isDenominatorValid(double denominator) {
+        return denominator != 0;
     }
 
 }
