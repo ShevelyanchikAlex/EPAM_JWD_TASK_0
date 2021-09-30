32d2e08fee9936246531bce9c33b6968c365f304
ShevelyanchikAlex <shevelyanchik01@mail.ru>
2021-09-30 13:31:25 +0300

    Refactor task7.

--- src/by/epam/task7/Task7.java
+++ src/by/epam/task7/Task7.java
@@ -1,21 +1,26 @@
 package by.epam.task7;
 
+import by.epam.services.reader.impl.ReaderImpl;
+import by.epam.services.reader.interfaces.Reader;
+import by.epam.services.writer.impl.WriterImpl;
+import by.epam.services.writer.interfaces.Writer;
+
 public class Task7 {
     public static void main(String[] args) {
-        double a = 1, b = 6;
-        double h = 2;
+        Reader reader = new ReaderImpl();
+        Writer writer = new WriterImpl();
 
-        printTable(a, b, h);
+        printTable(reader.readDouble(), reader.readDouble(), reader.readDouble(), writer);
     }
 
-    private static void printTable(double a, double b, double h) {
+    private static void printTable(double a, double b, double h, Writer writer) {
         final String line = "-------------------------";
-        System.out.println(line);
-        System.out.printf("|\t%4s\t|\t%5s\t|\n", 'x', 'y');
-        System.out.println(line);
+        writer.writeLn(line);
+        writer.writeF("|\t%4s\t|\t%5s\t|\n", 'x', 'y');
+        writer.writeLn(line);
         for (double i = a; i <= b; i += h) {
-            System.out.printf("|\t%4.1f\t|\t%4.3f\t|\n", i, Math.pow(Math.sin(i), 2) - Math.cos(2 * i));
+            writer.writeF("|\t%4.1f\t|\t%4.3f\t|\n", i, Math.pow(Math.sin(i), 2) - Math.cos(2 * i));
         }
-        System.out.println(line);
+        writer.writeLn(line);
     }
 }
