a8a9cde45ca0f4da9a990d3d35ed76bc665ccdc2
ShevelyanchikAlex <shevelyanchik01@mail.ru>
2021-09-30 13:30:14 +0300

    Refactor task1.

--- src/by/epam/task1/Task1.java
+++ src/by/epam/task1/Task1.java
@@ -1,29 +1,21 @@
 package by.epam.task1;
 
+import by.epam.services.reader.impl.ReaderImpl;
+import by.epam.services.reader.interfaces.Reader;
+import by.epam.services.writer.impl.WriterImpl;
+import by.epam.services.writer.interfaces.Writer;
+
 public class Task1 {
     public static void main(String[] args) {
-        System.out.println(isSumsEquals(2112));
+        Reader reader = new ReaderImpl();
+        Writer writer = new WriterImpl();
+
+        writer.write(isSumsEquals(reader.readInt()));
     }
 
     private static boolean isSumsEquals(int number) {
         int sum1 = number / 1000 + (number / 100) % 10;
         int sum2 = number % 10 + (number / 10) % 10;
-
-        return sum1 == sum2;
-    }
-
-    private static boolean isSumsEqualsCyclicalOption(int number) {
-        int sum1 = 0;
-        int sum2 = 0;
-
-        for (int i = 0; i < 4; i++) {
-            if (i < 2) {
-                sum1 += number % 10;
-            } else {
-                sum2 += number % 10;
-            }
-            number /= 10;
-        }
 
         return sum1 == sum2;
     }
