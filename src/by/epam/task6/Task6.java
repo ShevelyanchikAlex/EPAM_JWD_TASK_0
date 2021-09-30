78996a81f407cf12d745a5779ec439bf43851dde
ShevelyanchikAlex <shevelyanchik01@mail.ru>
2021-09-30 13:31:18 +0300

    Refactor task6.

--- src/by/epam/task6/Task6.java
+++ src/by/epam/task6/Task6.java
@@ -1,11 +1,31 @@
 package by.epam.task6;
 
+import by.epam.services.reader.impl.ReaderImpl;
+import by.epam.services.reader.interfaces.Reader;
+import by.epam.services.writer.impl.WriterImpl;
+import by.epam.services.writer.interfaces.Writer;
+
 public class Task6 {
     public static void main(String[] args) {
-        double max, min;
-        double sum;
-        double[] arr = {-2, 3, -10};
-        min = max = arr[0];
+        Reader reader = new ReaderImpl();
+        Writer writer = new WriterImpl();
+
+        double[] arr = inputNumbers(3, reader);
+        findSumMinAndMax(arr, writer);
+    }
+
+    private static double[] inputNumbers(int number, Reader reader) {
+        double[] arr = new double[number];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = reader.readDouble();
+        }
+        return arr;
+    }
+
+    private static void findSumMinAndMax(double[] arr, Writer writer) {
+        double min = arr[0];
+        double max = arr[0];
 
         for (double num : arr) {
             if (num < min) {
@@ -16,8 +36,7 @@ public class Task6 {
                 }
             }
         }
-
-        sum = min + max;
-        System.out.println(sum);
+        writer.writeLn(min + max);
     }
+
 }
